2dce90f0ebec7f2fc986c686e76007a6ae1d317e
Zakariae Nassiri <nassirizakariae113@gmail.com>
2026-05-20 10:40:35 +0100

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.cs
@@ -0,0 +1,27 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (string str in strs)
+        {
+            sb.Append($"{(char)str.Length}{str}");
+        }
+
+        return sb.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        List<string> decodedStrs = new List<string>();
+        int start = 0;
+        while(start < s.Length){
+            int str_len = (int) s[start];
+            start++;
+
+            decodedStrs.Add(s.Substring(start, str_len));
+
+            start += str_len;
+        }
+        return decodedStrs;
+   }
+}
